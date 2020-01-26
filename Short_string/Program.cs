f78a4397ab01a1228e70d2b6f0c4dd99f948ee0a
umohd5397 <umohd5397@gmail.com>
2020-01-26 23:56:04 +0530

    Add project files.

---
+++ Short_string/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Short_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string output = string.Empty;
+            string inputString = Console.ReadLine();
+            string[] splitStrings = inputString.Split(' ');
+            for (int i = splitStrings.Length - 1; i > -1; i--)
+            {
+                output = output + splitStrings[i] + " ";
+            }
+            Console.WriteLine(output);
+
+            Console.ReadKey();
+        }
+        public string ReverseFullSentence(string inputString)
+        {
+            string output = string.Empty;
+            string[] splitStrings = inputString.Split(' ');
+            for (int i = splitStrings.Length - 1; i > -1; i--)
+            {
+                output = output + splitStrings[i] + " ";
+            }
+            return output;
+        }
+    }
+}
